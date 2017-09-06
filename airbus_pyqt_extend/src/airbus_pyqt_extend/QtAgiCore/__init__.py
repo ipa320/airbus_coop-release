9949c728bbb761557d09ed4d55db3161f0dbb772
ipa-nhg <nhg@ipa.fhg.de>
2017-09-06 13:32:53 +0200

    Trimmed the branch to only the airbus_docgen sub directory

--- airbus_pyqt_extend/src/airbus_pyqt_extend/QtAgiCore/__init__.py
+++
@@ -1,8 +0,0 @@
-from topics import QAgiSubscriber
-from packages import get_pkg_dir_from_prefix, \
-                     get_ros_workspace_dir, \
-                     get_ros_workspace_src_dir, \
-                     QAgiPackages
-                     
-from resources import QAgiResources, \
-                      loadRsc, loadRes
