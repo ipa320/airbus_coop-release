d601c6f47d89f4484b15d8a7d7083f4c788c6a90
ipa-nhg <nhg@ipa.fhg.de>
2017-09-13 14:16:47 +0200

    Revert "Trimmed the branch to only the airbus_ssm_tutorial sub directory"
    This reverts commit 7b65be44134bbc6c6531b7f5f4f21b4179bcd63e.

---
+++ airbus_docgen/src/airbus_docgen/digraph/shape.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 Airbus
+# Copyright 2017 Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class SHAPE:
+    Box = "box"
+    Polygon = "polygon"
+    Ellipse = "ellipse"
+    Circle = "circle"
+    Plaintext = "plaintext"
+    Point = "point"
+    Egg = "egg"
+    Triangle = "triangle"
+    Plaintext = "plaintext"
+    Diamond = "diamond"
+    Trapezium = "trapezium"
+    Parallelogram = "parallelogram"
+    House = "house"
+    Hexagon = "hexagon"
+    Octagon = "octagon"
+    Doublecircle = "doublecircle"
+    Doubleoctagon = "doubleoctagon"
+    Tripleoctagon = "tripleoctagon"
+    Invtriangle = "invtriangle"
+    Invtrapezium = "invtrapezium"
+    
