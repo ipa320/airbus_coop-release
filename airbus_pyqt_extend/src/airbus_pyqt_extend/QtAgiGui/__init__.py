4172b77e12cbbd26b9bba7153a483a179b0a9f4c
ipa-nhg <nhg@ipa.fhg.de>
2017-09-13 14:16:42 +0200

    Trimmed the branch to only the airbus_coop sub directory

--- airbus_pyqt_extend/src/airbus_pyqt_extend/QtAgiGui/__init__.py
+++
@@ -1,5 +0,0 @@
-from joystick import QAgiJoystick, QAgiHJoystick, QAgiVJoystick
-from keyboard import QAgiKeyboard
-from messagebox import QAgiMessageBox
-from silderbutton import QAgiSilderButton
-from popup import QAgiPopup
